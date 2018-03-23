2ada564b12a242e242b5792abdd56ca7e9f6c1d7
salehinRafi <salehinrafi35@gmail.com>
2018-03-23 12:17:39 +0800

    Integrate Bot with FB Messenger

--- 2_Bot_in_FB_Messenger/fbeamer/index.js
+++ 2_Bot_in_FB_Messenger/fbeamer/index.js
@@ -1,4 +1,5 @@
 "use strict";
+const request = require("request");
 
 class FBeamer {
     constructor(config) {
@@ -21,11 +22,75 @@ class FBeamer {
         let { mode, verify_token, challenge } = req.query.hub; // use destrucuring object to access directly the value. 
 
         if (mode === "subscribe" && verify_token === this.VERIFY_TOKEN) {
+            console.log("Success connect to webook");
             return res.end(challenge); // send and end the response and send back the challenge code
         } else {
             console.log("Could not register webhook!");
             return res.status(403).end();
         }
+    }
+
+    // receive message request, response, callback
+    incoming(req, res, cb) {
+        // Extract body of the POST request
+        let data = req.body;
+        if (data.object === "page") {
+            // Iterate  through the page entry Array
+            data.entry.forEach(pageObj => {
+                // Iterate through the messaging Array
+                pageObj.messaging.forEach(msgEvent => {
+                    let messageObj = {
+                        sender: msgEvent.sender.id,
+                        timeOfMessage: msgEvent.timestamp,
+                        message: msgEvent.message
+                    };
+                    // execute callbakc function
+                    cb(messageObj);
+                });
+            });
+        }
+
+        res.send(200);
+    }
+
+    sendMessage(payload) {
+        return new Promise((resolve, reject) => {
+            // Create an HTTP POST request
+            request({
+                "uri": "https://graph.facebook.com/v2.6/me/messages",
+                "qs": {
+                    "access_token": this.PAGE_ACCESS_TOKEN
+                },
+                "method": "POST",
+                "json": payload
+            }, (error, response, body) => {
+                // Useful analyzing how the response went.Also accessing any request data that might have return.
+                if (!error && response.statusCode === 200) {
+                    console.log('Message sent!')
+                    resolve({
+                        messageId: body.message_id
+                    });
+                } else {
+                    console.error("Unable to send message:" + error);
+                    reject(error);
+                }
+            });
+        });
+    }
+
+    // Helper to test send message
+    txt(id, text) {
+        let obj = {
+            recipient: {
+                id
+            },
+            message: {
+                text
+            }
+        }
+
+        this.sendMessage(obj)
+            .catch(error => console.log(error));
     }
 }
 
--- 2_Bot_in_FB_Messenger/server.js
+++ 2_Bot_in_FB_Messenger/server.js
@@ -23,9 +23,20 @@ const f = new FBeamer(config);
 
 // Register the webhooks
 server.get('/', (req, res, next) => {
+    console.log("Connect Webhook");
     f.registerHook(req, res);
     return next(); // to ensure our control move on from this handler to next route handler in the apps. 
 });
 
+// Receive all incoming message
+server.post('/', (req, res, next) => {
+    f.incoming(req, res, msg => {
+        // Process message
+        console.log(msg);
+        //invoke function to test message.
+        f.txt(msg.sender, `Hey you just send ${msg.message.text}`)
+    });
+    return next();
+});
 
 server.listen(PORT, () => console.log(`Met running on port ${PORT}`));
