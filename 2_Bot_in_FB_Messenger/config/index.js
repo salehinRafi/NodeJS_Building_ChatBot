ec3587dc535fb60565b0ce4c09d69dbc2c0f2944
salehinRafi <salehinrafi35@gmail.com>
2018-03-27 12:27:43 +0800

    Update config/index.js

--- 2_Bot_in_FB_Messenger/config/index.js
+++ 2_Bot_in_FB_Messenger/config/index.js
@@ -4,7 +4,9 @@
 if (process.env.NODE_ENV === "production") { // production
     module.exports = {
         PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN,
-        VERIFY_TOKEN: process.env.VERIFY_TOKEN
+        VERIFY_TOKEN: process.env.VERIFY_TOKEN,
+        URI_MESSAGE: process.env.URI_MESSAGE,
+        URI_SUBSCRIBE: process.env.URI_SUBSCRIBE
     }
 } else { // development
     module.exports = require("./development.json");
